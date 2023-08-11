43c304b59e7a50150f82ef79f38bba038616da6b
Igor Steiner de Souza <52051101+IgorSteinerS@users.noreply.github.com>
2023-08-11 00:43:30 -0300

    Update JogoVelha.py
    Quando alguém ganha só uma mensagem é exibida.
    Dificuldade média concertada

--- Jogos/JogoVelha.py
+++ Jogos/JogoVelha.py
@@ -12,6 +12,14 @@ def jogar():
         not_num_used = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         player_turn = 1
         total_moves = 0
+        winning_sequences = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 
+                            [1, 4, 7], [2, 5, 8], [3, 6, 9],  
+                            [1, 5, 9], [3, 5, 7]]
+        blocking_sequences = [
+            [1,2,3], [1,3,2], [2,3,1], [4,5,6], [4,6,5], [5,6,4], [7,8,9], [7,9,8], [8,9,7],
+            [1,4,7], [1,7,4], [4,7,1], [2,5,8], [2,8,5], [5,8,2], [3,6,9], [3,7,5], [6,9,3],
+            [1,5,9], [1,9,5], [5,9,1], [3,5,7], [3,7,5], [5,7,3]
+        ]
 
         while True:
             mesmo = input("Você quer jogar mesmo? \n [S|N] \n ")
@@ -26,22 +34,16 @@ def jogar():
         if mesmo == "n" or mesmo == "N":
             break
 
-        def win(player, enemy, used_positions):
-            winning_sequences = [[1, 2, 3], [4, 5, 6], [7, 8, 9], 
-                                [1, 4, 7], [2, 5, 8], [3, 6, 9],  
-                                [1, 5, 9], [3, 5, 7]]            
-
+        def win(player, enemy, used_positions):          
             for sequence in winning_sequences:
                 if all(str(position) + player in used_positions for position in sequence):
-                    print(f"Player \"{player}\" wins!")
                     return 1 
                 elif all(str(position) + enemy in used_positions for position in sequence):
-                    print(f"Enemy \"{enemy}\" wins!")
                     return 2
                 elif total_moves >= 9:
                     return 3  
-            return None
-        
+            return None              
+          
         info = [["|1","|2|","3|"],
                 ["|4","|5|","6|"],
                 ["|7","|8|","9|"]]
@@ -100,6 +102,7 @@ def jogar():
                     print(*[grid[i] for i in range(7, 10)], sep="")
 
                     print(used_positions)
+
                     if win(player, enemy, used_positions) == 1:
                         winner = player
                         print(f"Player \"{player}\" wins!")
@@ -140,6 +143,49 @@ def jogar():
 
                     print(used_positions)
                 
+                    if win(player, enemy, used_positions) == 2:
+                        print(f"Enemy \"{enemy}\" wins!")
+                        winner = enemy
+                        break
+                    elif win(player, enemy, used_positions) == 3:
+                        winner = "tie"
+                        print(f"Everyone Loses!!!!")
+                        break
+                    else:
+                        player_turn = 1
+                        total_moves += 1
+
+                elif difficulty_level == "2":
+                    if not_num_used:
+                        enemy_choice = None
+                        for sequence in blocking_sequences:
+                            for spot in sequence[0:2]:
+                                if all(str(spot) + player in used_positions for spot in sequence[0:2]) and str(sequence[2]) + enemy not in used_positions:
+                                    enemy_choice = int(sequence[2])
+                                    break
+                        if enemy_choice is None:
+                            enemy_choice = random.choice(not_num_used)                           
+                    else:
+                        winner = "tie"
+                        print("Everyone Loses!!!!")
+                        break
+
+                    used_positions.append(str(enemy_choice) + f"{enemy}")
+                    num_used.append(enemy_choice)
+                    not_num_used.remove(enemy_choice)
+                    if enemy_choice == 1 or enemy_choice == 4 or enemy_choice == 7:
+                        grid.update({enemy_choice : f"|{enemy}"})
+                    elif enemy_choice == 2 or enemy_choice == 5 or enemy_choice == 8:
+                        grid.update({enemy_choice : f"|{enemy}|"})
+                    elif enemy_choice == 3 or enemy_choice == 6 or enemy_choice == 9:                        
+                        grid.update({enemy_choice : f"{enemy}|"})
+
+                    print(*[grid[i] for i in range(1, 4)], sep="")
+                    print(*[grid[i] for i in range(4, 7)], sep="")
+                    print(*[grid[i] for i in range(7, 10)], sep="")
+
+                    print(used_positions)
+
                     if win(player, enemy, used_positions) == 2:
                         print(f"Enemy \"{enemy}\" wins!")
                         winner = enemy
