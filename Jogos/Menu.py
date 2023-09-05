9a2a51ee77d08dad5c52ef4bed6485703b235ff9
Igor Steiner de Souza <52051101+IgorSteinerS@users.noreply.github.com>
2023-09-05 00:57:16 -0300

    Update and rename Jogos.py to Menu.py

---
+++ Jogos/Menu.py
@@ -0,0 +1,48 @@
+import Forca
+import JogoVelha
+
+class Jogos:
+    todos_jogos = {}
+
+    def __init__(self, id, name):
+        self.__id = id
+        self.__name = name
+        Jogos.todos_jogos[self.__id] = self
+                
+    def __str__(self):
+        return f"({self.__id}){self.__name}"    
+
+    def escolherJogo(self):
+        while True:
+            print("******************************")
+            print("*******Escolha o Jogo!********")
+            print("******************************")
+            for id, instance in Jogos.todos_jogos.items():
+                print(f"{instance}")
+            print("Digite \"Q\" para sair.")
+            jogo = input("Qual jogo? \n")
+            
+            try:
+                if jogo.upper() == "Q":
+                    return 
+                elif int(jogo) not in Jogos.todos_jogos.keys():
+                    print(f"Escolha Invalida: Jogo ({jogo}) não existe")
+                else:
+                    run = int(jogo)
+                    if run == 1:
+                        JogoVelha.jogar()
+                    elif run == 2:
+                        Forca.jogar()        
+            except ValueError:
+                print("Escolha invalida: Valor não numérico")
+
+                
+
+jogo1 = Jogos(1, "Jogo da Velha")
+jogo2 = Jogos(2, "Forca")
+
+
+jogo1.escolherJogo()
+
+if __name__ == '__main__':
+    Jogos.escolherJogoescolherJogo()
