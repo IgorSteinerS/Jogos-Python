9579b25b0c667e8d48f22c64aa611de826128e28
Igor Steiner de Souza <igorsteinerdesouza@gmail.com>
2025-04-24 19:04:16 -0300

    fix(menu): corrige bug nas importações em menu.py e aplica melhorias de estilo  Ajustado os caminhos de importação no menu.py que estavam quebrando a execução. Código formatado com Black e verificado com Pylint.  Closes #7

--- Jogos/menu.py
+++ Jogos/menu.py
@@ -1,52 +1,63 @@
-import Forca
-import JogoVelha
-import JogoVelhaMinimax
-import Pong
-import Snake 
+"""Menu de jogos
+1 - Jogo da Velha
+2 - Forca
+3 - Jogo da Velha Minimax
+4 - Pong
+5 - Snake
+"""
+
+import forca
+import jogo_velha
+import jogo_velha_minimax
+import pong
+import snake
+
 
 class Jogos:
+    """Classe que representa os jogos disponíveis no menu"""
+
     todos_jogos = {}
 
-    def __init__(self, id, name):
-        self.__id = id
+    def __init__(self, jogo_id, name):
+        self.__id = jogo_id
         self.__name = name
         Jogos.todos_jogos[self.__id] = self
-                
-    def __str__(self):
-        return f"({self.__id}){self.__name}"    
 
-    def escolherJogo(self):
+    def __str__(self):
+        return f"({self.__id}){self.__name}"
+
+    def escolher_jogo(self):
+        """Método que exibe o menu de jogos e permite ao usuário escolher um jogo para jogar"""
         while True:
             print("******************************")
             print("*******Escolha o Jogo!********")
             print("******************************")
-            for id, instance in Jogos.todos_jogos.items():
+            for _, instance in Jogos.todos_jogos.items():
                 print(f"{instance}")
             print("-" * 25)
-            print("Digite \"Q\" para sair.")
+            print('Digite "Q" para sair.')
             jogo = input("Qual jogo? \n")
-            
+
             try:
                 if jogo.upper() == "Q":
-                    return 
-                elif int(jogo) not in Jogos.todos_jogos.keys():
+                    return
+                if int(jogo) not in Jogos.todos_jogos:
                     print(f"Escolha Invalida: Jogo ({jogo}) não existe")
                 else:
                     run = int(jogo)
                     if run == 1:
-                        JogoVelha.jogar()
+                        jogo_velha.jogar()
                     elif run == 2:
-                        Forca.jogar()  
+                        forca.jogar()
                     elif run == 3:
-                        JogoVelhaMinimax.jogar()   
+                        jogo_velha_minimax.jogar()
                     elif run == 4:
-                        Pong.jogar()   
+                        pong.jogar()
                     elif run == 5:
-                        Snake.jogar()   
+                        snake.jogar()
             except ValueError:
                 print("Escolha invalida: Valor não numérico")
 
-                
 
 jogo1 = Jogos(1, "Jogo da Velha")
 jogo2 = Jogos(2, "Forca")
@@ -55,8 +66,5 @@ jogo4 = Jogos(4, "Pong")
 jogo5 = Jogos(5, "Snake")
 
 
-jogo1.escolherJogo()
-
-if __name__ == '__main__':
-    Jogos.escolherJogo()
-    
+if __name__ == "__main__":
+    jogo1.escolher_jogo()
